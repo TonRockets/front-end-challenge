5d21d9d2036308478218774d5d308857d85ceca3
TonRockets <wellington.pereira@al.forsoft.org.br>
2023-07-16 19:10:13 -0300

    feat: add desable autoComplete browser function in input component

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -4,9 +4,14 @@ import Styles from 'components/input/Input.module.scss'
 type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
     
 const Input: React.FC<InputProps> = (props: InputProps) => {
+    
+    const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => { 
+        event.target.readOnly = false
+     }
+    
     return (
         <div className={Styles.inputWrap}>
-               <input {...props} />
+               <input {...props} readOnly onFocus={enableInput} />
                <span className={Styles.status}>&#128545;</span>
             </div>
     )
