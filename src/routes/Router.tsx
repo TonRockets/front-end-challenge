ba38501d6fba625c173cba20a0f101c92b297852
TonRockets <wellington.pereira@al.forsoft.org.br>
2023-07-16 17:59:42 -0300

    refactor: unuseless component

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,10 +3,8 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import PageLoading from 'components/page/PageLoading';
 import Page from 'components/page/Page';
-
 import paths from 'routes/paths';
 
-const Home = lazy(async () => await import('pages/Home'));
 const Login = lazy(async () => await import('pages/login/Login'));
 const PageNotFound = lazy(async () => await import('pages/PageNotFound'));
 
